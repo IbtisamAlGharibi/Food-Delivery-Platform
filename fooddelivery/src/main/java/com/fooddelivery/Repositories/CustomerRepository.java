e81d2eaebb83e3135648cb580f04483edcceb68c
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-21 15:02:12 +0400

    adding method in CustomerRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/CustomerRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/CustomerRepository.java
@@ -1,10 +1,12 @@
 package com.fooddelivery.Repositories;
 
 import com.fooddelivery.Entities.Customer;
+import lombok.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 
 public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
@@ -13,5 +15,6 @@ public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
 
     @Query("Select c from Customer c where c.isActive=true and c.loyaltyPoints>=:points")
     List<Customer> findByLoyaltyPointsGreaterThanEqual(@Param("points") int points);
-
+    @Query("SELECT c FROM Customer c WHERE c.isActive=true AND c.createdDate BETWEEN :createdDate AND CURRENT_TIMESTAMP")
+    List<Customer> CustomerRegisterWithDateRange(@Param("createdDate") Date createdDate);
 }
