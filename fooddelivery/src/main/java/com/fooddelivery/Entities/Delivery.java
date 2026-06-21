6f4110ada62c120a52ff0be39f2a94be30edbfca
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-21 11:15:00 +0400

    Delivery class is craeted

---
+++ fooddelivery/src/main/java/com/fooddelivery/Entities/Delivery.java
@@ -0,0 +1,21 @@
+package com.fooddelivery.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Delivery {
+    private int trackingCode;
+    private String status;
+    private LocalDateTime assignedAt;
+    private  LocalDateTime pickedUpAt;
+    private LocalDateTime deliveredAt;
+    private Date createdDate;
+    private Date updatedDate;
+    private boolean  isActive;
+}
