059ebf57ef3c7042825f2db9305c9185bf670b33
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-21 16:09:40 +0400

    MenuItemRepository Done

---
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/MenuItemRepository.java
@@ -0,0 +1,22 @@
+package com.fooddelivery.Repositories;
+
+import com.fooddelivery.Entities.ComboMeal;
+import com.fooddelivery.Entities.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MenuItemRepository extends JpaRepository<MenuItem,Integer> {
+    @Query("select mi from MenuItem mi where mi.isActive= true and mi.restaurant.id=:id")
+    List<MenuItem> findByRestaurantId(@Param("id") Integer id);
+    @Query("select mi from MenuItem mi where mi.isAvailable= true and mi.restaurant.id=:id")
+    List<MenuItem> findByRestaurantIdAndIsAvailableTrue(@Param("id") Integer id);
+    @Query("select mi from MenuItem mi where mi.isVegetarian= true")
+    List<MenuItem> findByIsVegetarianTrue();
+    @Query("SELECT mi FROM MenuItem mi WHERE mi.isActive=true AND mi.price BETWEEN :min AND :max")
+    List<MenuItem> findByPriceBetween(double min, double max);
+    @Query("SELECT c FROM ComboMeal c JOIN c.menuItemList m WHERE c.isActive = true AND m.id = :menuItemId")
+    List<ComboMeal> findComboMealsByMenuItemId(@Param("menuItemId") Integer menuItemId);
+}
