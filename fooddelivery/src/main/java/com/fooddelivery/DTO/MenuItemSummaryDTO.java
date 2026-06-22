3ca30fdd0ccfe74b6f19752e7b87a591b9c4f9b0
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-22 14:28:27 +0400

    MenuItemSummaryDTO Done

---
+++ fooddelivery/src/main/java/com/fooddelivery/DTO/MenuItemSummaryDTO.java
@@ -0,0 +1,25 @@
+package com.fooddelivery.DTO;
+
+import com.fooddelivery.Entities.MenuItem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MenuItemSummaryDTO {
+    private int id;
+    private String name;
+    private double price;
+
+    public static MenuItemSummaryDTO fromEntity(MenuItem item) {
+
+        MenuItemSummaryDTO dto = new MenuItemSummaryDTO();
+        dto.setId(item.getId());
+        dto.setName(item.getName());
+        dto.setPrice(item.getPrice());
+
+        return dto;
+    }
+}
