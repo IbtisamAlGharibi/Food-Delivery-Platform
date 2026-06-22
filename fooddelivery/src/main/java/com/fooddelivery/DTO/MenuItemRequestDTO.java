8b9943a98054e2ba2250fff51bfe5d718c4e3318
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-22 14:46:18 +0400

    Validation for MenuItemRequestDTO Done

--- fooddelivery/src/main/java/com/fooddelivery/DTO/MenuItemRequestDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/DTO/MenuItemRequestDTO.java
@@ -1,6 +1,9 @@
 package com.fooddelivery.DTO;
 
 import com.fooddelivery.Entities.MenuItem;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +13,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class MenuItemRequestDTO {
+    @NotBlank
     private String name;
+    @NotBlank
     private String description;
+    @PositiveOrZero
+    @DecimalMin("0.0")
     private double price;
     private boolean isAvailable;
     private boolean isVegetarian;
+    @DecimalMin("0.0")
     private double calories;
 
     public MenuItem toEntity() {
