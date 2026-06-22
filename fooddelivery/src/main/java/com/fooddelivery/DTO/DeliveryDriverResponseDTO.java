c109f7d681e30b10d4710547b22b0df8e14c3f82
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-22 14:10:23 +0400

    DeliveryDriverSummaryDTO Done

---
+++ fooddelivery/src/main/java/com/fooddelivery/DTO/DeliveryDriverResponseDTO.java
@@ -0,0 +1,43 @@
+package com.fooddelivery.DTO;
+
+import com.fooddelivery.Entities.DeliveryDriver;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryDriverResponseDTO {
+    private int driverId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int phone;
+    private String vehicleType;
+    private String vehiclePlate;
+    private String currentLat;
+    private String currentLng;
+    private boolean isOnline;
+    private boolean  isActive;
+
+
+    public static DeliveryDriverResponseDTO fromEntity(DeliveryDriver deliveryDriver) {
+
+        DeliveryDriverResponseDTO dto = new DeliveryDriverResponseDTO();
+
+        dto.setDriverId(deliveryDriver.getDriverCode());
+        dto.setFirstName(deliveryDriver.getFirstName());
+        dto.setLastName(deliveryDriver.getLastName());
+        dto.setEmail(deliveryDriver.getEmail());
+        dto.setPhone(deliveryDriver.getPhone());
+        dto.setVehicleType(deliveryDriver.getVehicleType());
+        dto.setVehiclePlate(deliveryDriver.getVehiclePlate());
+        dto.setCurrentLat(deliveryDriver.getCurrentLat());
+        dto.setCurrentLng(deliveryDriver.getCurrentLng());
+        dto.setOnline(deliveryDriver.isOnline());
+        dto.setActive(dto.isActive());
+
+        return dto;
+    }
+}
--- fooddelivery/src/main/java/com/fooddelivery/DTO/DeliveryDriverSummaryDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/DTO/DeliveryDriverSummaryDTO.java
@@ -1,5 +1,6 @@
 package com.fooddelivery.DTO;
 
+import com.fooddelivery.Entities.Delivery;
 import com.fooddelivery.Entities.DeliveryDriver;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,33 +9,21 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class DeliverDriverResponseDTO {
+public class DeliveryDriverSummaryDTO {
     private int driverId;
     private String firstName;
     private String lastName;
-    private String email;
-    private int phone;
     private String vehicleType;
-    private String vehiclePlate;
-    private String currentLat;
-    private String currentLng;
     private boolean isOnline;
     private boolean  isActive;
 
-
-    public static DeliverDriverResponseDTO fromEntity(DeliveryDriver deliveryDriver) {
-
-        DeliverDriverResponseDTO dto = new DeliverDriverResponseDTO();
+    public static DeliveryDriverResponseDTO fromEntity(DeliveryDriver deliveryDriver) {
+        DeliveryDriverResponseDTO dto = new DeliveryDriverResponseDTO();
 
         dto.setDriverId(deliveryDriver.getDriverCode());
         dto.setFirstName(deliveryDriver.getFirstName());
         dto.setLastName(deliveryDriver.getLastName());
-        dto.setEmail(deliveryDriver.getEmail());
-        dto.setPhone(deliveryDriver.getPhone());
         dto.setVehicleType(deliveryDriver.getVehicleType());
-        dto.setVehiclePlate(deliveryDriver.getVehiclePlate());
-        dto.setCurrentLat(deliveryDriver.getCurrentLat());
-        dto.setCurrentLng(deliveryDriver.getCurrentLng());
         dto.setOnline(deliveryDriver.isOnline());
         dto.setActive(dto.isActive());
 
