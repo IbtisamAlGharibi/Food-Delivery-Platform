7e33b54b2eaebef4eea5cd4633cdd1029a28bc88
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-22 11:47:54 +0400

    RestaurantSummaryDTO Done

---
+++ fooddelivery/src/main/java/com/fooddelivery/DTO/RestaurantSummaryDTO.java
@@ -0,0 +1,33 @@
+package com.fooddelivery.DTO;
+
+import com.fooddelivery.Entities.Restaurant;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RestaurantSummaryDTO {
+    private int id;
+    private String name;
+    private String description;
+    private String cuisineType;
+    private LocalDateTime openingTime;
+    private LocalDateTime closingTime;
+    private boolean  isActive;
+
+    public static RestaurantResponseDTO fromEntity(Restaurant restaurant){
+        RestaurantResponseDTO dto = new RestaurantResponseDTO();
+        dto.setId(restaurant.getId());
+        dto.setName(restaurant.getName());
+        dto.setDescription(restaurant.getDescription());
+        dto.setCuisineType(restaurant.getCuisineType());
+        dto.setOpeningTime(restaurant.getOpeningTime());
+        dto.setClosingTime(restaurant.getClosingTime());
+        dto.setActive(restaurant.isActive());
+
+        return dto;
+    }
+}
