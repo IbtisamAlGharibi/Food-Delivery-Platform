22ae37628643dfd3f024c1bd7a47413cd5e7ea9e
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-22 14:44:32 +0400

    Validation for DeliveryDriverRequestDTO Done

--- fooddelivery/src/main/java/com/fooddelivery/DTO/DeliveryDriverRequestDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/DTO/DeliveryDriverRequestDTO.java
@@ -1,6 +1,9 @@
 package com.fooddelivery.DTO;
 
 import com.fooddelivery.Entities.DeliveryDriver;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +13,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class DeliveryDriverRequestDTO {
+    @NotBlank
     private String firstName;
+    @NotBlank
     private String lastName;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+    @Pattern(regexp = "^\\+?[0-9]{8,15}$", message = "Phone number must contain 8-15 digits")
     private int phone;
+    @NotBlank(message = "Password is required")
     private String passwordHash;
     private String vehicleType;
     private String vehiclePlate;
