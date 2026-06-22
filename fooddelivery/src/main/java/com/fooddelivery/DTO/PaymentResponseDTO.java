d08b2ec82d4e10460fcdd39bf3320e25a1760201
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-22 14:17:47 +0400

    PaymentResponseDTO Done

---
+++ fooddelivery/src/main/java/com/fooddelivery/DTO/PaymentResponseDTO.java
@@ -0,0 +1,40 @@
+package com.fooddelivery.DTO;
+
+import com.fooddelivery.Entities.Payment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentResponseDTO {
+
+    private int id;
+    private int transactionRef;
+    private String paymentMethod;
+    private String status;
+    private double amount;
+    private LocalDateTime processedAt;
+    private Date createdDate;
+    private Date updatedDate;
+
+    public static PaymentResponseDTO fromEntity(Payment payment) {
+
+        PaymentResponseDTO dto = new PaymentResponseDTO();
+
+        dto.setId(payment.getId());
+        dto.setTransactionRef(payment.getTransactionRef());
+        dto.setPaymentMethod(payment.getPaymentMethod());
+        dto.setStatus(payment.getStatus());
+        dto.setAmount(payment.getAmount());
+        dto.setProcessedAt(payment.getProcessedAt());
+        dto.setCreatedDate(payment.getCreatedDate());
+        dto.setUpdatedDate(payment.getUpdatedDate());
+
+        return dto;
+    }
+}
