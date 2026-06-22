6b5c65911ed6703b3aeaed8a6e05fd44dc2a60cd
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-22 12:15:55 +0400

    DeliveryRequestDTO Done

---
+++ fooddelivery/src/main/java/com/fooddelivery/DTO/DeliveryRequestDTO.java
@@ -0,0 +1,36 @@
+package com.fooddelivery.DTO;
+
+import com.fooddelivery.Entities.Delivery;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class DeliveryRequestDTO {
+    private String status;
+    private LocalDateTime assignedAt;
+    private  LocalDateTime pickedUpAt;
+    private LocalDateTime deliveredAt;
+
+    public Delivery toEntity() {
+        Delivery delivery = new Delivery();
+        delivery.setStatus(status);
+        delivery.setAssignedAt(assignedAt);
+        delivery.setPickedUpAt(pickedUpAt);
+        delivery.setDeliveredAt(deliveredAt);
+
+        return delivery;
+    }
+
+    public void applyTo(Delivery delivery){
+        delivery.setStatus(status);
+        delivery.setAssignedAt(assignedAt);
+        delivery.setPickedUpAt(pickedUpAt);
+        delivery.setDeliveredAt(deliveredAt);
+    }
+
+}
