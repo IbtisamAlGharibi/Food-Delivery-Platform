d42e01e983c9cbf05b7ec47acbe89fffaffc639d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-23 15:30:00 +0400

    autoAssignDriver Done

--- fooddelivery/src/main/java/com/fooddelivery/Services/DeliveryService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/DeliveryService.java
@@ -10,6 +10,8 @@ import com.fooddelivery.Repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DeliveryService {
     DeliveryRepository deliveryRepository;
@@ -25,6 +27,27 @@ public class DeliveryService {
         DeliveryDriver driver = deliveryRepository.findById(driverId)
                 .orElseThrow(() -> new ResourceNotFoundException("Driver not found")).getDeliveryDriver();
 
+        Delivery delivery = new Delivery();
+        delivery.setOrder(order);
+        delivery.setDeliveryDriver(driver);
+        delivery.setStatus("ASSIGNED");
+        delivery = deliveryRepository.save(delivery);
+
+        return DeliveryResponseDTO.fromEntity(delivery);
+    }
+    public DeliveryResponseDTO autoAssignDriver(Integer orderId){
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found"));
+        List<Delivery> deliveries = deliveryRepository.findAll();
+
+        DeliveryDriver driver = new DeliveryDriver();
+        for(Delivery delivery : deliveries){
+            if(delivery.getDeliveryDriver() != null &&
+                    delivery.getDeliveryDriver().isOnline()){
+                driver = delivery.getDeliveryDriver();
+                break;
+            }
+        }
         Delivery delivery = new Delivery();
         delivery.setOrder(order);
         delivery.setDeliveryDriver(driver);
