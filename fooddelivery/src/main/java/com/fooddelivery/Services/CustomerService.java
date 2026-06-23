de61184d9c08e9dd0138477304f291c8d50c8d58
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-23 09:59:51 +0400

    createCustomer Done

--- fooddelivery/src/main/java/com/fooddelivery/Services/CustomerService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/CustomerService.java
@@ -1,5 +1,8 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.DTO.RequestDTOs.CustomerRequestDTO;
+import com.fooddelivery.DTO.ResponseDTOs.CustomerResponseDTO;
+import com.fooddelivery.Entities.Customer;
 import com.fooddelivery.Repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +13,16 @@ public class CustomerService {
     @Autowired
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+    }
+    public CustomerResponseDTO createCustomer(CustomerRequestDTO dto){
+        Customer customer = dto.toEntity();
+        customer.setFirstName(dto.getFirstName());
+        customer.setLastName(dto.getLastName());
+        customer.setPhone(dto.getPhone());
+        customer.setEmail(dto.getEmail());
+        customer.setPasswordHash(dto.getPasswordHash());
+        customer= customerRepository.save(customer);
+        return CustomerResponseDTO.fromEntity(customer);
     }
 
 }
