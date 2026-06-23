25ae333c3a1334a7eb67bf89dd238763f005d8ac
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-23 10:44:35 +0400

    createRestaurant Done

--- fooddelivery/src/main/java/com/fooddelivery/Services/RestaurantService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/RestaurantService.java
@@ -1,5 +1,10 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.DTO.RequestDTOs.RestaurantRequestDTO;
+import com.fooddelivery.DTO.ResponseDTOs.RestaurantResponseDTO;
+import com.fooddelivery.Entities.Restaurant;
+import com.fooddelivery.Entities.RestaurantOwner;
+import com.fooddelivery.Exceptions.ResourceNotFoundException;
 import com.fooddelivery.Repositories.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,4 +17,22 @@ public class RestaurantService {
         this.restaurantRepository = restaurantRepository;
     }
 
+    public RestaurantResponseDTO createRestaurant(RestaurantRequestDTO dto, Integer ownerId){
+        RestaurantOwner owner = restaurantRepository.findById(ownerId)
+                .orElseThrow(() -> new ResourceNotFoundException("Owner not found")).getRestaurantOwner();
+
+        Restaurant restaurant = dto.toEntity();
+        restaurant.setName(dto.getName());
+        restaurant.setDescription(dto.getDescription());
+        restaurant.setCuisineType(dto.getCuisineType());
+        restaurant.setOpeningTime(dto.getOpeningTime());
+        restaurant.setClosingTime(dto.getClosingTime());
+        restaurant.setMinOrderAmount(dto.getMinOrderAmount());
+        restaurant.setDeliveryFee(dto.getDeliveryFee());
+        restaurant.setAcceptingOrders(dto.isAcceptingOrders());
+        restaurant.setRestaurantOwner(owner);
+        restaurant = restaurantRepository.save(restaurant);
+
+        return RestaurantResponseDTO.fromEntity(restaurant);
+    }
 }
