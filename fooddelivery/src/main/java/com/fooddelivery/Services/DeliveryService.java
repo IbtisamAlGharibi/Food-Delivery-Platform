f3d8edf838c75029c0d510afef29624f9a06c8d4
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-23 15:14:01 +0400

    assignDriverToOrder Done

--- fooddelivery/src/main/java/com/fooddelivery/Services/DeliveryService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/DeliveryService.java
@@ -1,5 +1,10 @@
 package com.fooddelivery.Services;
 
+import com.fooddelivery.DTO.ResponseDTOs.DeliveryResponseDTO;
+import com.fooddelivery.Entities.Delivery;
+import com.fooddelivery.Entities.DeliveryDriver;
+import com.fooddelivery.Entities.Order;
+import com.fooddelivery.Exceptions.ResourceNotFoundException;
 import com.fooddelivery.Repositories.DeliveryRepository;
 import com.fooddelivery.Repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,5 +19,18 @@ public class DeliveryService {
         this.deliveryRepository = deliveryRepository;
         this.orderRepository = orderRepository;
     }
+    public DeliveryResponseDTO assignDriverToOrder(Integer orderId, Integer driverId){
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found"));
+        DeliveryDriver driver = deliveryRepository.findById(driverId)
+                .orElseThrow(() -> new ResourceNotFoundException("Driver not found")).getDeliveryDriver();
 
+        Delivery delivery = new Delivery();
+        delivery.setOrder(order);
+        delivery.setDeliveryDriver(driver);
+        delivery.setStatus("ASSIGNED");
+        delivery = deliveryRepository.save(delivery);
+
+        return DeliveryResponseDTO.fromEntity(delivery);
+    }
 }
