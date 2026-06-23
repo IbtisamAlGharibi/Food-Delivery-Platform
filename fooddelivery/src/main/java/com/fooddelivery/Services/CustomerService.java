bb30f4f56d2395cd9c61512fa410183cfbdd9a41
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-23 10:13:30 +0400

    addAddress Done

--- fooddelivery/src/main/java/com/fooddelivery/Services/CustomerService.java
+++ fooddelivery/src/main/java/com/fooddelivery/Services/CustomerService.java
@@ -2,9 +2,11 @@ package com.fooddelivery.Services;
 
 import com.fooddelivery.DTO.RequestDTOs.CustomerAddressRequestDTO;
 import com.fooddelivery.DTO.RequestDTOs.CustomerRequestDTO;
+import com.fooddelivery.DTO.ResponseDTOs.CustomerAddressResponseDTO;
 import com.fooddelivery.DTO.ResponseDTOs.CustomerResponseDTO;
 import com.fooddelivery.Entities.Customer;
 import com.fooddelivery.Entities.CustomerAddress;
+import com.fooddelivery.Exceptions.ResourceNotFoundException;
 import com.fooddelivery.Repositories.CustomerAddressRepository;
 import com.fooddelivery.Repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,4 +52,20 @@ public class CustomerService {
         return CustomerResponseDTO.fromEntity(customer);
     }
 
+    public CustomerAddressResponseDTO addAddress(Integer customerId,CustomerAddressRequestDTO address) {
+
+        Customer customer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found"));
+
+        CustomerAddress customerAddress = address.toEntity();
+        customerAddress.setStreet(address.getStreet());
+        customerAddress.setCity(address.getCity());
+        customerAddress.setBuilding(address.getBuilding());
+        customerAddress.setDefault(address.isDefault());
+        customerAddress = customerAddressRepository.save(customerAddress);
+        customer.getCustomerAddressList().add(customerAddress);
+        customerRepository.save(customer);
+
+        return CustomerAddressResponseDTO.fromEntity(customerAddress);
+    }
 }
