7efd3c210689597da368ab96441bd0e41cc129e5
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-23 15:51:24 +0400

    addition in OrderItemRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/OrderItemRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/OrderItemRepository.java
@@ -1,6 +1,7 @@
 package com.fooddelivery.Repositories;
 
+import com.fooddelivery.Entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository {
+public interface OrderItemRepository extends JpaRepository<OrderItem,Integer> {
 }
