3c83f5c98fbd7cadffe03e2f974dd53969cc5ad4
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-23 09:04:37 +0400

    ComboMealRequestDTO Validation Done

--- fooddelivery/src/main/java/com/fooddelivery/DTO/RequestDTOs/ComboMealRequestDTO.java
+++ fooddelivery/src/main/java/com/fooddelivery/DTO/RequestDTOs/ComboMealRequestDTO.java
@@ -1,6 +1,9 @@
 package com.fooddelivery.DTO.RequestDTOs;
 
 import com.fooddelivery.Entities.ComboMeal;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ComboMealRequestDTO {
+    @NotBlank
     private String comboName;
     private String description;
+    @PositiveOrZero
+    @DecimalMin("0.0")
     private double totalPrice;
     private boolean isAvailable;
 
