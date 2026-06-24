debf0ef994c54fa983990d71f9be7daba779b314
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-24 10:25:24 +0400

    @PostMapping Done

--- fooddelivery/src/main/java/com/fooddelivery/Controllers/DriverController.java
+++ fooddelivery/src/main/java/com/fooddelivery/Controllers/DriverController.java
@@ -1,7 +1,14 @@
 package com.fooddelivery.Controllers;
 
+import com.fooddelivery.DTO.RequestDTOs.DeliveryDriverRequestDTO;
+import com.fooddelivery.DTO.ResponseDTOs.DeliveryDriverResponseDTO;
 import com.fooddelivery.Services.DeliveryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +20,9 @@ public class DriverController {
     public DriverController(DeliveryService deliveryService) {
         this.deliveryService = deliveryService;
     }
-
+    @PostMapping
+    public ResponseEntity<DeliveryDriverResponseDTO> createDriver(@Valid @RequestBody DeliveryDriverRequestDTO dto) {
+        DeliveryDriverResponseDTO driver = deliveryService.createDriver(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(driver);
+    }
 }
