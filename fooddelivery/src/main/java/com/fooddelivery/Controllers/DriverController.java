66e0fd2600bf608c9d0ec99e653209f6f9895d19
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-24 10:26:56 +0400

    GetMapping Done

--- fooddelivery/src/main/java/com/fooddelivery/Controllers/DriverController.java
+++ fooddelivery/src/main/java/com/fooddelivery/Controllers/DriverController.java
@@ -28,4 +28,8 @@ public class DriverController {
     public ResponseEntity<List<DeliveryDriverResponseDTO>> getAllDrivers() {
         return ResponseEntity.ok(deliveryService.getAllDrivers());
     }
+    @GetMapping("/online")
+    public ResponseEntity<List<DeliveryDriverResponseDTO>> getOnlineDrivers() {
+        return ResponseEntity.ok(deliveryService.getOnlineDrivers());
+    }
 }
