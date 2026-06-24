4833daa3801fdb452f9400c394e1325339364d86
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-24 12:06:50 +0400

    PostMapping Done

--- fooddelivery/src/main/java/com/fooddelivery/Controllers/OrderController.java
+++ fooddelivery/src/main/java/com/fooddelivery/Controllers/OrderController.java
@@ -1,9 +1,16 @@
 package com.fooddelivery.Controllers;
 
+import com.fooddelivery.DTO.ResponseDTOs.OrderResponseDTO;
 import com.fooddelivery.Services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -12,5 +19,11 @@ public class OrderController {
     @Autowired
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
+    }
+    @PostMapping("/customer/{customerId}/restaurant/{restaurantId}")
+    public ResponseEntity<OrderResponseDTO> createOrder(@PathVariable Integer customerId,
+            @PathVariable Integer restaurantId) {
+        OrderResponseDTO order = orderService.createOrder(customerId, restaurantId, Collections.emptyList());
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
     }
 }
