166e8d4c1d5fc7ef567803f73ec3dc2253678051
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-24 10:26:15 +0400

    GetMapping Done

--- fooddelivery/src/main/java/com/fooddelivery/Controllers/DriverController.java
+++ fooddelivery/src/main/java/com/fooddelivery/Controllers/DriverController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/drivers")
@@ -24,5 +23,9 @@ public class DriverController {
     public ResponseEntity<DeliveryDriverResponseDTO> createDriver(@Valid @RequestBody DeliveryDriverRequestDTO dto) {
         DeliveryDriverResponseDTO driver = deliveryService.createDriver(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(driver);
+    }
+    @GetMapping
+    public ResponseEntity<List<DeliveryDriverResponseDTO>> getAllDrivers() {
+        return ResponseEntity.ok(deliveryService.getAllDrivers());
     }
 }
