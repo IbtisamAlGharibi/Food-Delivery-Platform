c981afc0bfdf2da073ff4fe9a602b95c33ada434
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-24 14:55:03 +0400

    PostMapping Done

--- fooddelivery/src/main/java/com/fooddelivery/Controllers/DeliveryController.java
+++ fooddelivery/src/main/java/com/fooddelivery/Controllers/DeliveryController.java
@@ -1,7 +1,11 @@
 package com.fooddelivery.Controllers;
 
+import com.fooddelivery.DTO.ResponseDTOs.DeliveryResponseDTO;
 import com.fooddelivery.Services.DeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,9 @@ public class DeliveryController {
     @Autowired
     public DeliveryController(DeliveryService deliveryService) {
         this.deliveryService = deliveryService;
+    }
+    @PostMapping("/order/{orderId}/assignManual/{driverId}")
+    public ResponseEntity<DeliveryResponseDTO> assignManual(@PathVariable Integer orderId, @PathVariable Integer driverId) {
+        return ResponseEntity.ok(deliveryService.assignDriverToOrder(orderId, driverId));
     }
 }
