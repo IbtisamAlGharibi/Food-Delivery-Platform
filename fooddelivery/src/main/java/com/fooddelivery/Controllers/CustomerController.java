ca659c5d59e07222c696767d7dfb93c8dd8f9ac7
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-24 09:34:27 +0400

    PostMapping Done

--- fooddelivery/src/main/java/com/fooddelivery/Controllers/CustomerController.java
+++ fooddelivery/src/main/java/com/fooddelivery/Controllers/CustomerController.java
@@ -1,7 +1,14 @@
 package com.fooddelivery.Controllers;
 
+import com.fooddelivery.DTO.RequestDTOs.CustomerRequestDTO;
+import com.fooddelivery.DTO.ResponseDTOs.CustomerResponseDTO;
 import com.fooddelivery.Services.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +20,9 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
-
+    @PostMapping
+    public ResponseEntity<CustomerResponseDTO> createCustomer(@Valid @RequestBody CustomerRequestDTO dto) {
+        CustomerResponseDTO customer = customerService.createCustomer(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(customer);
+    }
 }
