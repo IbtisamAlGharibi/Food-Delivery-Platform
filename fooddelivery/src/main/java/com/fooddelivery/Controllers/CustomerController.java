e5000bd468acc4a4a01df44dc4cac33773273fef
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-24 09:26:35 +0400

    CustomerController is created

---
+++ fooddelivery/src/main/java/com/fooddelivery/Controllers/CustomerController.java
@@ -0,0 +1,17 @@
+package com.fooddelivery.Controllers;
+
+import com.fooddelivery.Services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+    CustomerService customerService;
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+}
