86cd9a206e193123cb96e32602b2649a999717ca
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-24 15:31:56 +0400

    findByOrderId is added in the PymentRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/PaymentRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/PaymentRepository.java
@@ -2,7 +2,9 @@ package com.fooddelivery.Repositories;
 
 import com.fooddelivery.Entities.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-
+    @Query("select p from Pyment p where p.order.orderCode=:orderId")
+    public Payment findByOrderId(Integer orderId);
 }
