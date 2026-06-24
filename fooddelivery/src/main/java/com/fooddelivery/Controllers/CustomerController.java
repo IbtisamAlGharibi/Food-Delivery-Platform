e628b3a8089e51f767b482689ebd343cfd88eee7
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-24 09:35:39 +0400

    GetMapping Done

--- fooddelivery/src/main/java/com/fooddelivery/Controllers/CustomerController.java
+++ fooddelivery/src/main/java/com/fooddelivery/Controllers/CustomerController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/customers")
@@ -24,5 +23,9 @@ public class CustomerController {
     public ResponseEntity<CustomerResponseDTO> createCustomer(@Valid @RequestBody CustomerRequestDTO dto) {
         CustomerResponseDTO customer = customerService.createCustomer(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(customer);
+    }
+    @GetMapping
+    public ResponseEntity<List<CustomerResponseDTO>> getAllCustomers() {
+        return ResponseEntity.ok(customerService.getAllCustomers());
     }
 }
