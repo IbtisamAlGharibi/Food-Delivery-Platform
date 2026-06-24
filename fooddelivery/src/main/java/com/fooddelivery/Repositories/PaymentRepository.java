9c8cf3a00a33d0cafcaf41afc8a0c153ec2bcfb9
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-24 16:08:31 +0400

    fixing some errors in the PaymentRepository

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/PaymentRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/PaymentRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-    @Query("select p from Pyment p where p.order.orderCode=:orderId")
+    @Query("select p from Payment p where p.order.orderCode=:orderId")
     public Payment findByOrderId(Integer orderId);
 }
