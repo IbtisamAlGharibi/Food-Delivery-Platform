728b6a0823e34f092297cb8b929efb5bdeafdc48
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-25 11:07:55 +0400

    getTopSellingItems is added

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/OrderItemRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/OrderItemRepository.java
@@ -2,6 +2,11 @@ package com.fooddelivery.Repositories;
 
 import com.fooddelivery.Entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem,Integer> {
+    @Query(" select oi from OrderItem oi where oi.order.restaurant.id=:restaurantId order by oi.quantity desc")
+    List<OrderItem> getTopSellingItems(Integer restaurantId);
 }
