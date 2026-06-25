465c2c60091a623f063cd39e1bfa9d49f5a7b950
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-25 10:42:27 +0400

    searchCustomers is added

--- fooddelivery/src/main/java/com/fooddelivery/Repositories/CustomerRepository.java
+++ fooddelivery/src/main/java/com/fooddelivery/Repositories/CustomerRepository.java
@@ -2,7 +2,8 @@ package com.fooddelivery.Repositories;
 
 import com.fooddelivery.DTO.ResponseDTOs.CustomerResponseDTO;
 import com.fooddelivery.Entities.Customer;
-import lombok.Data;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -22,4 +23,6 @@ public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
     List<CustomerResponseDTO> getAllCustomers();
     @Query(value = " select c from Customer c where c.isActive=true order by c.loyaltyPoints desc limit 10")
     List<Customer> getTopLoyalCustomers();
+    @Query("select c from Customer c where c.isActive=true and c.firstName=:nam ")
+    Page<Customer> findByFirstName(String name, Pageable pageable);
 }
